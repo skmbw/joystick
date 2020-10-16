f32682f4eed10f2e59cf3b579e9fb9e6032c3da3
skmbw <tongku2008@126.com>
2020-10-16 17:20:33 +0800

    为SplashActivity添加两个按钮，启动对应的工作

--- app/src/main/java/com/xuershangda/joystick/SplashActivity.java
+++ app/src/main/java/com/xuershangda/joystick/SplashActivity.java
@@ -1,7 +1,10 @@
 package com.xuershangda.joystick;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
+import com.xuershangda.joystick.nav.NavigationActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        findViewById(R.id.navigation).setOnClickListener(v -> {
+            Intent intent = new Intent(this, NavigationActivity.class);
+            startActivity(intent);
+        });
+
+        findViewById(R.id.operation).setOnClickListener(v -> {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        });
     }
 }
