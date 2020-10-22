363b7f5b6db9aa783d516e76692588b5b1e3a16e
skmbw <tongku2008@126.com>
2020-10-22 16:27:31 +0800

    添加json库，添加通用http请求方法

--- app/src/main/java/com/xuershangda/joystick/nav/NavigationActivity.java
+++ app/src/main/java/com/xuershangda/joystick/nav/NavigationActivity.java
@@ -1,17 +1,31 @@
 package com.xuershangda.joystick.nav;
 
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 
+import com.alibaba.fastjson.JSON;
 import com.xuershangda.joystick.R;
 import com.xuershangda.joystick.listener.FingerTouchViewListener;
 import com.xuershangda.joystick.view.FingerPaintImageView;
 
+import java.io.File;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicBoolean;
 
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+
 public class NavigationActivity extends AppCompatActivity {
+    public static final String BOUNDARY = "--tda67ajd9km3zs05dha991piq90cm0bf43vd--";
     private static final String TAG = "NavigationActivity";
     private FingerPaintImageView mFingerPaintImageView;
     private AtomicBoolean startPosition = new AtomicBoolean(false);
@@ -95,5 +109,91 @@ public class NavigationActivity extends AppCompatActivity {
             this.mFingerPaintImageView.setInEditMode(true);
             this.startPosition.set(false);
         });
+    }
+
+    private void imageSrc() {
+//        this.mFingerPaintImageView.setImageDrawable();
+//        this.mFingerPaintImageView.setImageBitmap();
+//        this.mFingerPaintImageView.setImageResource();
+//        this.mFingerPaintImageView.setImageURI();
+    }
+
+    private void getImage() {
+        BitmapFactory.decodeByteArray(null, 0, 1);
+        BitmapFactory.decodeStream(null);
+    }
+
+    /**
+     * OkHttpClient 封装了参数设置
+     * @param url url
+     * @param params 参数
+     * @param callback OkHttpClient Call对象 回调
+     */
+    protected void call(String url, Map<String, Object> params, Callback callback) {
+        Log.i(TAG, "call: OkHttp call");
+        OkHttpClient okHttpClient = new OkHttpClient();
+        MultipartBody.Builder builder = new MultipartBody.Builder(BOUNDARY).setType(MultipartBody.FORM);
+
+        for (Map.Entry<String, Object> entry : params.entrySet()) {
+            String name = entry.getKey();
+            Object value = entry.getValue();
+            if (value instanceof List) {
+                List listValue = (List) value;
+                for (Object obj : listValue) {
+                    if (obj instanceof File) {
+                        File file = (File) obj;
+                        RequestBody body = RequestBody.create(MediaType.parse("application/octet-stream"), file);
+                        builder.addFormDataPart(name, file.getName(), body);
+                    } else {
+                        builder.addFormDataPart(name, obj.toString());
+                    }
+                }
+            } else {
+                if (value instanceof File) {
+                    File file = (File) value;
+                    RequestBody body = RequestBody.create(MediaType.parse("application/octet-stream"), file);
+                    builder.addFormDataPart(name, file.getName(), body);
+                } else {
+                    builder.addFormDataPart(name, value.toString());
+                }
+            }
+        }
+
+        Log.i(TAG, "BasicActivity call: okhttp 构建参数完成");
+
+        // Multipart body must have at least one part
+        // 如果一个参数都没有就不能添加body了，防止没有登录的情况
+        Request.Builder requestBuilder = new Request.Builder();
+        requestBuilder.url(url);
+
+        Request request = requestBuilder.build();
+        Call call = okHttpClient.newCall(request);
+        call.enqueue(callback);
+    }
+
+    /**
+     * OkHttpClient 封装了post json 请求的参数设置
+     *
+     * @param url url
+     * @param object 参数
+     * @param callback OkHttpClient Call对象 回调
+     */
+    protected void postJson(String url, Object object, Callback callback) {
+        Log.i(TAG, "OkHttp post json.");
+        OkHttpClient okHttpClient = new OkHttpClient();
+
+        Request.Builder requestBuilder = new Request.Builder();
+        requestBuilder.url(url);
+
+        byte[] jsonBody = JSON.toJSONBytes(object);
+        RequestBody body = RequestBody.create(MediaType.parse("application/json; charset=UTF-8"), jsonBody);
+        requestBuilder.post(body);
+
+        Log.i(TAG, "postJson: OkHttp 构建参数完成");
+
+        Request request = requestBuilder.build();
+
+        Call call = okHttpClient.newCall(request);
+        call.enqueue(callback);
     }
 }
