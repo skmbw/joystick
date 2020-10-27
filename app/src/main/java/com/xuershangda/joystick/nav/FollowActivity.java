b9cbee7339714e4953c662ada530d30008edef52
skmbw <tongku2008@126.com>
2020-10-27 16:23:42 +0800

    添加两个activity，用于设置跟随和巡检

---
+++ app/src/main/java/com/xuershangda/joystick/nav/FollowActivity.java
@@ -0,0 +1,21 @@
+package com.xuershangda.joystick.nav;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.xuershangda.joystick.R;
+
+/**
+ * 跟随Activity，开始跟随、停止跟随、切换跟随
+ *
+ * @author yinlei
+ * @since 2020-10-27
+ */
+public class FollowActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_follow);
+    }
+}
---
+++ app/src/main/java/com/xuershangda/joystick/nav/InspectionActivity.java
@@ -0,0 +1,21 @@
+package com.xuershangda.joystick.nav;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.xuershangda.joystick.R;
+
+/**
+ * 巡检Activity，设置巡检的点
+ *
+ * @author yinlei
+ * @since 2020-10-27
+ */
+public class InspectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inspection);
+    }
+}
